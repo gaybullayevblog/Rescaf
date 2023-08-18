f0419581f0563aadf8d23cefec4373bfe3a726f5
gaybullayevblog <gaybullayevblog@gmail.com>
2023-08-18 13:07:22 +0500

    done

---
+++ base/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# from .models import Home
+# Create your views here.
+
+
+class HomeView(TemplateView):
+    # model = Home
+    template_name = "pages/home.html"
+
+
+class AboutView(TemplateView):
+    # model = Home
+    template_name = "pages/about.html"
